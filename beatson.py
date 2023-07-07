00c701e1b3a2483e1d7c454f8c561785b62c3464
Stella Eva Tsiapali <2526934t@student.gla.ac.uk>
2023-07-07 13:44:22 +0100

    Local (simple) search functionality added

--- beatson.py
+++ beatson.py
@@ -1,5 +1,7 @@
+import re
 import streamlit as st
 import pandas as pd
+import numpy as np
 import xml.etree.ElementTree as ET
 from Bio import Entrez
 from collections import defaultdict
@@ -13,7 +15,11 @@ base_url = "https://www.ncbi.nlm.nih.gov/bioproject/"
 max_search_results = 10
 search_msg = "Getting search results..."
 loading_msg = "Loading project data..."
+gsheet_url_proj = st.secrets["private_gsheets_url_proj"]
+gsheet_url_pub = st.secrets["private_gsheets_url_pub"]
 gsheet_url_annot = st.secrets["private_gsheets_url_annot"]
+project_columns = ["uid", "acc", "title", "name", "description", "datatype", "scope", "organism", "publications", "labels"]
+pub_columns = ["pmid", "title", "abstract", "mesh", "keywords"]
 id_col = "UID"
 annot_col = "Project_labels"
 project_col = "Project_title"
@@ -27,7 +33,9 @@ css = r'''
 '''
 st.markdown(css, unsafe_allow_html=True)
 
-
+def uid_to_url(uid):
+    return base_url + str(uid) + "/"
+    
 @st.cache_resource(show_spinner=loading_msg)
 def connect_gsheets_api():
     connection = connect(
@@ -72,9 +80,15 @@ def delete_annotation(gsheet_url_annot, project_uid):
             WHERE {id_col} = {project_uid}
             """
     connection.execute(delete) 
-
-def uid_to_url(uid):
-    return base_url + str(uid) + "/"
+    
+@st.cache_resource(show_spinner=loading_msg)
+def load_data(gsheet_url, _columns):
+    query = f'SELECT * FROM "{gsheet_url}"'
+    executed_query = connection.execute(query)
+    df = pd.DataFrame(executed_query.fetchall())
+    if not df.empty:
+        df.columns = _columns
+    return df
     
 def submit_labels():
     new = st.session_state.get("new", "")
@@ -144,7 +158,7 @@ def parse_xml(element):
     return tag, data_dict
     
 @st.cache_data(show_spinner=search_msg)
-def search_with_api(search_terms):
+def api_search(search_terms):
     search_terms = [term.strip() for term in search_terms.split() if term.strip()]
     # TODO: find synonyms
     ids = esearch(term="+AND+".join(search_terms))
@@ -166,81 +180,108 @@ def search_with_api(search_terms):
             )
             search_df.set_index(id_col, inplace=True)
             return search_df
+            
+@st.cache_data(show_spinner=search_msg)
+def local_search(search_terms, project_df, pub_df):
+    search_terms = [term.strip() for term in search_terms.split() if term.strip()]
+    search_expr = r"(\b(" + "|".join(search_terms) + r")\b)"
+    # TODO: find synonyms
+    
+    raw_counts = np.column_stack([project_df.astype(str)[col].str.count(search_expr, flags=re.IGNORECASE) for col in project_df])
+    total_counts = np.sum(raw_counts, axis=1)
+    mask = np.where(total_counts > 0, True, False)
+    search_df = project_df.loc[mask]
+    
+    search_df.sort_index(axis=0, key=lambda col: col.map(lambda i: total_counts[i]), ascending=False, inplace=True, ignore_index=True)
+    return search_df
 
     
 st.title("BioProject Annotation")
 
 st.header("Search")
 search = st.text_input("Search:", label_visibility="collapsed")
-search_df = None
+connection = connect_gsheets_api()
+project_df = load_data(gsheet_url_proj, project_columns)
+pub_df = load_data(gsheet_url_pub, pub_columns)
 
 if search:
-    search_df = search_with_api(search)
+    search_df = local_search(search, project_df, pub_df)
     if search_df is not None and not search_df.empty:
+        st.write(f"Search results for '{search}':")
         st.dataframe(
             search_df,
             use_container_width=True,
-            column_config={
-                id_col: st.column_config.TextColumn(),
-                project_col: st.column_config.TextColumn(),
-                url_col: st.column_config.LinkColumn(width="small", validate="^"+base_url+"[0-9]*/$")
-            },
+            hide_index=True,
+            column_config={col : st.column_config.TextColumn() for col in project_columns},
         )
     else:
         st.write("No search results.")
+        st.dataframe(
+            project_df,
+            use_container_width=True,
+            hide_index=True,
+            column_config={col : st.column_config.TextColumn() for col in project_columns},
+        )
+else:
+    st.dataframe(
+        project_df,
+        use_container_width=True,
+        hide_index=True,
+        column_config={col : st.column_config.TextColumn() for col in project_columns},
+    )
 
 st.header("Annotate")
-connection = connect_gsheets_api()
 
-annot_df = load_annotations(gsheet_url_annot)
-col1, col2 = st.columns(2)
+
+# annot_df = load_annotations(gsheet_url_annot)
+# col1, col2 = st.columns(2)
     
-with col1:
-    project_options = set(annot_df.index.tolist())
-    if search_df is not None:
-        project_options.update(set(search_df.index.tolist()))
-    project_uid = st.selectbox("Project UID:", [""] + sorted(list(project_options)), key="project_uid")
-    if project_uid:
-        project_uid = int(project_uid)
+# with col1:
+    # project_options = set(annot_df.index.tolist())
+    # if search_df is not None:
+        # project_options.update(set(search_df["id_col"].tolist()))
+    # project_uid = st.selectbox("Project UID:", [""] + sorted(list(project_options)), key="project_uid")
+    # if project_uid:
+        # project_uid = int(project_uid)
 
-with col2:
-    label_options = set()
-    if not annot_df.empty:
-        for l in annot_df[annot_col]:
-            label_options.update(set(l.split(delimiter)))
-        label_options = sorted(list(label_options))
+# with col2:
+    # label_options = set()
+    # if not annot_df.empty:
+        # for l in annot_df[annot_col]:
+            # label_options.update(set(l.split(delimiter)))
+        # label_options = sorted(list(label_options))
         
-    original_labels = None
-    if project_uid in annot_df.index:
-        original_labels = annot_df.at[project_uid, annot_col].split(delimiter)
+    # original_labels = None
+    # if project_uid in annot_df.index:
+        # original_labels = annot_df.at[project_uid, annot_col].split(delimiter)
         
-    with st.form(key="Annotate"):
-        if label_options:
-            labels = st.multiselect("Labels:", label_options, default=original_labels, key="labels")
-            new = st.text_input("Or create new:", placeholder="Or create new (comma-separated)", label_visibility="collapsed", key="new")
-        else:
-            labels = ""
-            new = st.text_input("Labels:", placeholder="Create new (comma-separated)", key="new")
-        submit_button = st.form_submit_button("Submit", on_click=submit_labels)
+    # with st.form(key="Annotate"):
+        # if label_options:
+            # labels = st.multiselect("Labels:", label_options, default=original_labels, key="labels")
+            # new = st.text_input("Or create new:", placeholder="Or create new (comma-separated)", label_visibility="collapsed", key="new")
+        # else:
+            # labels = ""
+            # new = st.text_input("Labels:", placeholder="Create new (comma-separated)", key="new")
+        # submit_button = st.form_submit_button("Submit", on_click=submit_labels)
         
 
-def start_learning():
-    return
+# def start_learning():
+    # return
 
-if not annot_df.empty:      
-    st.header("Review")
-    st.write(f"{annot_df.shape[0]} project{'' if annot_df.shape[0] == 1 else 's'} annotated. {len(label_options)} disctinct label{'' if len(label_options) == 1 else 's'} used.")
-    st.dataframe(
-        annot_df,
-        use_container_width=True,
-        column_config={
-            id_col: st.column_config.TextColumn(),
-            annot_col: st.column_config.TextColumn(),
-            url_col: st.column_config.LinkColumn(width="small", validate="^"+base_url+"[0-9]*/$")
-        },
-    )
+# if not annot_df.empty:      
+    # st.header("Review")
+    # st.write(f"{annot_df.shape[0]} project{'' if annot_df.shape[0] == 1 else 's'} annotated. {len(label_options)} disctinct label{'' if len(label_options) == 1 else 's'} used.")
+    # st.dataframe(
+        # annot_df,
+        # use_container_width=True,
+        # column_config={
+            # id_col: st.column_config.TextColumn(),
+            # annot_col: st.column_config.TextColumn(),
+            # url_col: st.column_config.LinkColumn(width="small", validate="^"+base_url+"[0-9]*/$")
+        # },
+    # )
     
-    st.header("Learn")
-    st.write("This does nothing.")
-    learn_button = st.button("Start", on_click=start_learning)
+    # st.header("Learn")
+    # st.write("This does nothing.")
+    # learn_button = st.button("Start", on_click=start_learning)
     
